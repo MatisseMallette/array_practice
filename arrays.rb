5c408dd3c0dd149b09fe3d1a628e4904382f537c
Matisse Mallette <matissemallette@gmail.com>
2022-08-03 12:44:17 -0700

    Use four different methods to manipulate arrays

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,16 @@
-array_1 = ["my", "name", "is", "jeff"]
-array_2 = [0, 1, 2, 3]
-array_3 = [0.0, 1.0, 2.0, 3.0]
-array_4 = [false, true, false, true]
+strings = ["my", "name", "is", "jeff"]
+integers = [0, 1, 2, 3]
+floats = [0.0, 1.0, 2.0, 3.0]
+booleans = [false, true, false, true]
+
+# the pop(num_of_elements) method removes a certain number of elements from the end of an array and returns the removed elements
+puts strings.pop()
+
+# the push(elements) method adds any number of elements to the end of an array and returns the array itself
+puts integers.push([4, 5, 6], 7, 8, 9)
+
+# the shift(num_of_elements) method removes a certain number of elements from the beginning of an array and returns the removed elements
+puts floats.shift()
+
+# the unshift(elements) method adds any number of elements to the beginning of an array and returns the array itself
+puts booleans.unshift(true)
