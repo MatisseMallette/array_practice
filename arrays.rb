1bb80c0a2cf36340df667a0aa55e58cf4b4ff0fd
Matisse Mallette <matissemallette@gmail.com>
2022-08-03 12:57:22 -0700

    Show my understanding of some array functions and array indices

--- arrays.rb
+++ arrays.rb
@@ -14,3 +14,16 @@ puts floats.shift()
 
 # the unshift(elements) method adds any number of elements to the beginning of an array and returns the array itself
 puts booleans.unshift(true)
+
+# the take(n) method returns the first n elements from an array
+puts strings.take(3)
+# it is worth pointing out that unlike pop, push, shift and unshift, take does not alter the contents of an array
+puts strings # this array remains the same even after calling the take method
+
+# the elements of an array can be addressed by typing the name of the array they belong to followed by square brackets contaning an index
+# elements of arrays are indexed beginning at 0
+# so to print the first element of the strings array  type
+puts strings[0]
+
+# to print the fourth element type
+puts strings[3]
